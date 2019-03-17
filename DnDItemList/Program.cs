60869e31bf206e11dde81a72bbba6b4a1f321370
Quin Hagerty <hagertyq@gmail.com>
2019-03-17 15:12:22 -0400

    Added my CSV and read from it.

--- DnDItemList/Program.cs
+++ DnDItemList/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DnDItemList
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+        
+            Console.WriteLine("Here");
+            var fileName = @"C: \Users\hager\Desktop\DnDItemList\DnDItemListCSV.csv";
+            var fileContents = ReadFile(fileName);
+            Console.WriteLine(fileContents);
+            Console.ReadLine();
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
