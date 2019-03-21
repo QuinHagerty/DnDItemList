0953480e1327d89e2e5768c1b2548fe40f067ad5
Quin Hagerty <hagertyq@gmail.com>
2019-03-20 20:00:10 -0400

    read my csv into a list and printed the list item to the console

--- DnDItemList/Program.cs
+++ DnDItemList/Program.cs
@@ -11,20 +11,44 @@ namespace DnDItemList
     {
         public static void Main(string[] args)
         {
-        
-            Console.WriteLine("Here");
             var fileName = @"C: \Users\hager\Desktop\DnDItemList\DnDItemListCSV.csv";
             var fileContents = ReadFile(fileName);
             Console.WriteLine(fileContents);
             Console.ReadLine();
         }
 
-        public static string ReadFile(string fileName)
+        public static List<Weapon> ReadFile(string fileName)
         {
+            List<Weapon> DnDWeaponList = new List<Weapon>();
+            //return reader.ReadToEnd();
+
             using (var reader = new StreamReader(fileName))
             {
-                return reader.ReadToEnd();
+                while(!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var data = line.Split(',');
+                    var Weapon = new Weapon
+                    {
+                        Name = data[0],
+                        Category = data[1],
+                        Damage = data[2],
+                        DamageType = data[3],
+                        ItemRarity = data[4],
+                        Properties = data[5],
+                        Range = data[6],
+                        Weight = data[7]
+                    };
+                    // prevent duplicates
+                    DnDWeaponList.Add(Weapon);
+                }
+                foreach (Weapon weapon in DnDWeaponList)
+                {
+                    Console.WriteLine(weapon.Name + " " + weapon.Category + " " + weapon.Damage + " " + weapon.DamageType + " " + weapon.ItemRarity + " " + weapon.Properties + " " + weapon.Range + " " + weapon.Weight + ".");
+                }
             }
+
+            return DnDWeaponList;
         }
     }
 }
---
+++ DnDItemList/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDItemList
+{
+    class Weapon
+    {
+        public string Name;
+        public string Category;
+        public string Damage;
+        public string DamageType;
+        public string ItemRarity;
+        public string Properties;
+        public string Range;
+        public string Weight;
+    }
+}
