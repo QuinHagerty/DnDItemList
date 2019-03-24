5317822f8cf6c6a98cfc08c2885e1dca28c1cea2
Quin Hagerty <hagertyq@gmail.com>
2019-03-24 13:47:55 -0400

    Creted a for loop to write added weapons back to a CSV string so I can save the new list over the old item list.

---
+++ DnDItemList/NewWeapon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDItemList
+{
+    class NewWeapon
+    {
+        public static List<Weapon> ReadFile(string fileName)
+        {
+            List<Weapon> DnDWeaponList = new List<Weapon>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var data = line.Split(',');
+                    var Weapon = new Weapon
+                    {
+                        Name = data[0],
+                        Category = data[1],
+                        Damage = data[2],
+                        DamageType = data[3],
+                        ItemRarity = data[4],
+                        Properties = data[5],
+                        Range = data[6],
+                        Weight = data[7]
+                    };
+                    // prevent duplicates
+                    DnDWeaponList.Add(Weapon);
+                }
+                foreach (Weapon weapon in DnDWeaponList)
+                {
+                    Console.WriteLine(
+                        "Name:" + weapon.Name +
+                        " Category:" + weapon.Category +
+                        " Damage:" + weapon.Damage +
+                        " Damage Type:" + weapon.DamageType +
+                        " Item Rarity:" + weapon.ItemRarity +
+                        " Properties:" + weapon.Properties +
+                        " Range:" + weapon.Range +
+                        " Weight:" + weapon.Weight +
+                        ".");
+                }
+            }
+
+            return DnDWeaponList;
+        }
+
+        public static void WriteFile(List<Weapon> NewWeapons, string path)
+        {
+            // using (var fileStream = new FileStream(String.Format("Person{0}.txt", Id), FileMode.OpenOrCreate))
+            using (var streamWriter = new StreamWriter(path))
+            {
+                string WeaponString = string.Empty;
+
+                foreach (Weapon weapon in NewWeapons)
+                {
+                    WeaponString +=
+                    weapon.Name + "," +
+                    weapon.Category + "," +
+                    weapon.Damage + "," +
+                    weapon.DamageType + "," +
+                    weapon.ItemRarity + "," +
+                    weapon.Properties + "," +
+                    weapon.Range + "," +
+                    weapon.Weight +
+                    "/n";
+
+                    //append
+                }
+
+                //write
+            }
+        }
+    }
+}
--- DnDItemList/Program.cs
+++ DnDItemList/Program.cs
@@ -12,43 +12,9 @@ namespace DnDItemList
         public static void Main(string[] args)
         {
             var fileName = @"C: \Users\hager\Desktop\DnDItemList\DnDItemListCSV.csv";
-            var fileContents = ReadFile(fileName);
+            var fileContents = NewWeapon.ReadFile(fileName);
             Console.WriteLine(fileContents);
             Console.ReadLine();
-        }
-
-        public static List<Weapon> ReadFile(string fileName)
-        {
-            List<Weapon> DnDWeaponList = new List<Weapon>();
-            //return reader.ReadToEnd();
-
-            using (var reader = new StreamReader(fileName))
-            {
-                while(!reader.EndOfStream)
-                {
-                    var line = reader.ReadLine();
-                    var data = line.Split(',');
-                    var Weapon = new Weapon
-                    {
-                        Name = data[0],
-                        Category = data[1],
-                        Damage = data[2],
-                        DamageType = data[3],
-                        ItemRarity = data[4],
-                        Properties = data[5],
-                        Range = data[6],
-                        Weight = data[7]
-                    };
-                    // prevent duplicates
-                    DnDWeaponList.Add(Weapon);
-                }
-                foreach (Weapon weapon in DnDWeaponList)
-                {
-                    Console.WriteLine(weapon.Name + " " + weapon.Category + " " + weapon.Damage + " " + weapon.DamageType + " " + weapon.ItemRarity + " " + weapon.Properties + " " + weapon.Range + " " + weapon.Weight + ".");
-                }
-            }
-
-            return DnDWeaponList;
         }
     }
 }
