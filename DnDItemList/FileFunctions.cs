a445f7291833c60630c0c2149f4569a2c075cea2
Quin Hagerty <hagertyq@gmail.com>
2019-03-27 19:56:22 -0400

    Fixed a bug in my persisting data and added more options for the user.

---
+++ DnDItemList/FileFunctions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDItemList
+{
+    class FileFunctions
+    {
+        public static List<Weapon> ReadFile(string fileName)
+        {
+            List<Weapon> DnDWeaponList = new List<Weapon>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var columns = line.Split(',');
+                    var Weapon = new Weapon()
+                    {
+                        Name = columns[0],
+                        Category = columns[1],
+                        Damage = columns[2],
+                        DamageType = columns[3],
+                        ItemRarity = columns[4],
+                        Properties = columns[5],
+                        Range = columns[6],
+                        Weight = columns[7]
+                    };
+                    
+                    
+                    // prevent duplicates
+                    DnDWeaponList.Add(Weapon);
+                }
+            }
+            return DnDWeaponList;
+        }
+
+        public static void WriteFile(List<Weapon> NewWeapons, string path)
+        {
+            string WeaponString = string.Empty;
+
+            foreach (Weapon weapon in NewWeapons)
+            {
+                WeaponString +=
+                weapon.Name + "," +
+                weapon.Category + "," +
+                weapon.Damage + "," +
+                weapon.DamageType + "," +
+                weapon.ItemRarity + "," +
+                weapon.Properties + "," +
+                weapon.Range + "," +
+                weapon.Weight +
+                Environment.NewLine;
+            }
+            File.WriteAllText(path, WeaponString);
+        }
+    }
+}
--- DnDItemList/Program.cs
+++ DnDItemList/Program.cs
@@ -11,10 +11,65 @@ namespace DnDItemList
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the DnD 5E item database");
+            Console.WriteLine("Would you like to 1. View the items, 2. add an item, or 3. edit an existing item? Please type the number of your option.");
+            userResponse = Console.ReadLine();
+
+        switch (userResponse)
+        {
+            case 1: ViewItems
+                foreach (Weapon weapon in DnDWeaponList)
+                {
+                    Console.WriteLine(
+                        "Name:" + weapon.Name +
+                        " Category:" + weapon.Category +
+                        " Damage:" + weapon.Damage +
+                        " Damage Type:" + weapon.DamageType +
+                        " Item Rarity:" + weapon.ItemRarity +
+                        " Properties:" + weapon.Properties +
+                        " Range:" + weapon.Range +
+                        " Weight:" + weapon.Weight +
+                        ".");
+                }
+                break;
+            case 2:
+                Console.WriteLine();
+                break;
+            case 3:
+                Console.WriteLine();
+                break;
+
+        }
+
+
             var fileName = @"C: \Users\hager\Desktop\DnDItemList\DnDItemListCSV.csv";
-            var fileContents = NewWeapon.ReadFile(fileName);
-            Console.WriteLine(fileContents);
-            Console.ReadLine();
+            List<Weapon> fileContents = FileFunctions.ReadFile(fileName);
+
+            FileFunctions.WriteFile(NewWeapon(fileContents), fileName);
+        }
+
+        private static List<Weapon> NewWeapon(List<Weapon> fileContents)
+        {
+            var newWeapon = new Weapon();
+            Console.WriteLine("Enter new weapon name: ");
+            newWeapon.Name = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Category");
+            newWeapon.Category = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Damage");
+            newWeapon.Damage = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Damage Type");
+            newWeapon.DamageType = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Item Rarity");
+            newWeapon.ItemRarity = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Properties");
+            newWeapon.Properties = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Range");
+            newWeapon.Range = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Weight");
+            newWeapon.Weight = Console.ReadLine();
+
+            fileContents.Add(newWeapon);
+            return fileContents;
         }
     }
 }
