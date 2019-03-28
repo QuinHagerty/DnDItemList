b83022841d0214978e72765f377586103887a52c
Quin Hagerty <hagertyq@gmail.com>
2019-03-28 14:27:20 -0400

    Moved all my Methods out of my Main and fixed my ViewData so that it actually works.

--- DnDItemList/Program.cs
+++ DnDItemList/Program.cs
@@ -11,65 +11,27 @@ namespace DnDItemList
     {
         public static void Main(string[] args)
         {
+            var fileName = @"C: \Users\hager\Desktop\DnDItemList\DnDItemListCSV.csv";
+
             Console.WriteLine("Welcome to the DnD 5E item database");
             Console.WriteLine("Would you like to 1. View the items, 2. add an item, or 3. edit an existing item? Please type the number of your option.");
-            userResponse = Console.ReadLine();
+            var userResponse = Console.ReadLine();
+            int userNumber = Convert.ToInt16(userResponse);
 
-        switch (userResponse)
-        {
-            case 1: ViewItems
-                foreach (Weapon weapon in DnDWeaponList)
-                {
-                    Console.WriteLine(
-                        "Name:" + weapon.Name +
-                        " Category:" + weapon.Category +
-                        " Damage:" + weapon.Damage +
-                        " Damage Type:" + weapon.DamageType +
-                        " Item Rarity:" + weapon.ItemRarity +
-                        " Properties:" + weapon.Properties +
-                        " Range:" + weapon.Range +
-                        " Weight:" + weapon.Weight +
-                        ".");
-                }
-                break;
-            case 2:
-                Console.WriteLine();
-                break;
-            case 3:
-                Console.WriteLine();
-                break;
+            switch (userNumber)
+            {
+                case 1:
+                    ReadWrite.ViewData(fileName);
+                    break;
+                //case 2:
+                //    ReadWrite.AddData();
+                //    break;
+                //case 3:
+                //    ReadWrite.EditData();
+                //    break;
 
-        }
-
-
-            var fileName = @"C: \Users\hager\Desktop\DnDItemList\DnDItemListCSV.csv";
-            List<Weapon> fileContents = FileFunctions.ReadFile(fileName);
-
-            FileFunctions.WriteFile(NewWeapon(fileContents), fileName);
-        }
-
-        private static List<Weapon> NewWeapon(List<Weapon> fileContents)
-        {
-            var newWeapon = new Weapon();
-            Console.WriteLine("Enter new weapon name: ");
-            newWeapon.Name = Console.ReadLine();
-            Console.WriteLine("Enter " + newWeapon.Name + " Category");
-            newWeapon.Category = Console.ReadLine();
-            Console.WriteLine("Enter " + newWeapon.Name + " Damage");
-            newWeapon.Damage = Console.ReadLine();
-            Console.WriteLine("Enter " + newWeapon.Name + " Damage Type");
-            newWeapon.DamageType = Console.ReadLine();
-            Console.WriteLine("Enter " + newWeapon.Name + " Item Rarity");
-            newWeapon.ItemRarity = Console.ReadLine();
-            Console.WriteLine("Enter " + newWeapon.Name + " Properties");
-            newWeapon.Properties = Console.ReadLine();
-            Console.WriteLine("Enter " + newWeapon.Name + " Range");
-            newWeapon.Range = Console.ReadLine();
-            Console.WriteLine("Enter " + newWeapon.Name + " Weight");
-            newWeapon.Weight = Console.ReadLine();
-
-            fileContents.Add(newWeapon);
-            return fileContents;
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ DnDItemList/ReadWrite.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDItemList
+{
+    class ReadWrite
+    {
+        public static List<Weapon> ViewData(string fileName)
+        {
+
+            //List<Weapon> fileContents = ReadWrite.ViewData(fileName);
+
+            List<Weapon> DnDWeaponList = new List<Weapon>();
+
+            using (var reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var columns = line.Split(',');
+                    var Weapon = new Weapon()
+                    {
+                        Name = columns[0],
+                        Category = columns[1],
+                        Damage = columns[2],
+                        DamageType = columns[3],
+                        ItemRarity = columns[4],
+                        Properties = columns[5],
+                        Range = columns[6],
+                        Weight = columns[7]
+                    };
+                    DnDWeaponList.Add(Weapon);
+                }
+                foreach (Weapon weapon in DnDWeaponList)
+                {
+                    Console.Write(
+                    weapon.Name + "," +
+                    weapon.Category + "," +
+                    weapon.Damage + "," +
+                    weapon.DamageType + "," +
+                    weapon.ItemRarity + "," +
+                    weapon.Properties + "," +
+                    weapon.Range + "," +
+                    weapon.Weight +
+                    Environment.NewLine);
+                }
+            }
+            return DnDWeaponList;
+        }
+
+        public static void AddData(List<Weapon> NewWeapons, string path)
+        {
+            string WeaponString = string.Empty;
+
+            foreach (Weapon weapon in NewWeapons)
+            {
+                WeaponString +=
+                weapon.Name + "," +
+                weapon.Category + "," +
+                weapon.Damage + "," +
+                weapon.DamageType + "," +
+                weapon.ItemRarity + "," +
+                weapon.Properties + "," +
+                weapon.Range + "," +
+                weapon.Weight +
+                Environment.NewLine;
+            }
+            File.WriteAllText(path, WeaponString);
+        }
+
+        private static List<Weapon> AddData(List<Weapon> fileContents)
+        {
+            var newWeapon = new Weapon();
+            Console.WriteLine("Enter new weapon name: ");
+            newWeapon.Name = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Category");
+            newWeapon.Category = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Damage");
+            newWeapon.Damage = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Damage Type");
+            newWeapon.DamageType = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Item Rarity");
+            newWeapon.ItemRarity = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Properties");
+            newWeapon.Properties = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Range");
+            newWeapon.Range = Console.ReadLine();
+            Console.WriteLine("Enter " + newWeapon.Name + " Weight");
+            newWeapon.Weight = Console.ReadLine();
+
+            fileContents.Add(newWeapon);
+            return fileContents;
+        }
+
+    }
+}
